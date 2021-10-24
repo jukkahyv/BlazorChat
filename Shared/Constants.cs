576752bb1daaeb81f77f5541fdfe43ae67dc1ded
Jukka <jukkahyv@gmail.com>
2021-10-24 12:02:11 +0300

    comments

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -3,6 +3,9 @@ namespace BlazorWebAssemblySignalRApp.Shared
 {
     public static class Constants
     {
+        /// <summary>
+        /// Maximum number of users in a group.
+        /// </summary>
         public const int MaxMemberCount = 20;
     }
 }
--- Shared/MessageNames.cs
+++ Shared/MessageNames.cs
@@ -1,9 +1,14 @@
 ﻿
 namespace BlazorWebAssemblySignalRApp.Shared
 {
+
+    /// <summary>
+    /// SignalR message names.
+    /// </summary>
     public static class MessageNames
     {
         public const string RefreshGroups = nameof(RefreshGroups);
         public const string ReceiveMessage = nameof(ReceiveMessage);
     }
+
 }
